e6be32e44c67775cf91c90fa22d9cc04b6a59a2d
Wybe Buising <wybe@kiener.nl>
2020-07-07 16:36:45 +0200

    Added persisting of Shipping Options

--- src/Service/ShippingOptions/ShippingOptionsService.php
+++ src/Service/ShippingOptions/ShippingOptionsService.php
@@ -4,6 +4,7 @@
 namespace Kiener\KienerMyParcel\Service\ShippingOptions;
 
 
+use Kiener\KienerMyParcel\Core\Content\Shipment\ShipmentEntity;
 use Kiener\KienerMyParcel\Core\Content\ShippingOption\ShippingOptionEntity;
 use Psr\Log\LoggerInterface;
 use Shopware\Core\Checkout\Order\OrderEntity;
@@ -11,6 +12,7 @@ use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\Uuid\Uuid;
 
 class ShippingOptionsService
 {
@@ -37,6 +39,48 @@ class ShippingOptionsService
     {
         $this->logger = $logger;
         $this->shippingOptionsRepository = $shippingOptionsRepository;
+    }
+
+    /**
+     * @param array   $params
+     * @param Context $context
+     *
+     * @return ShippingOptionEntity|null
+     */
+    public function createOrUpdateShippingOptions(array $params, Context $context): ?ShippingOptionEntity
+    {
+        // Create a new shipment entity if no id is present
+        if (!isset($params[ShippingOptionEntity::FIELD_ID])) {
+            $params[ShippingOptionEntity::FIELD_ID] = Uuid::randomHex();
+        }
+
+        // Upsert the data in the database
+        $event = $this->shippingOptionsRepository->upsert([$params], $context);
+
+        // Check for errors
+        if (!empty($event->getErrors())) {
+            $this->logger->error(
+                implode(', ', $event->getErrors()),
+                $event->getErrors()
+            );
+
+            return null;
+        }
+
+        return $this->getShippingOptions($params[ShippingOptionEntity::FIELD_ID], $context);
+    }
+
+    /**
+     * @param string  $id
+     * @param Context $context
+     *
+     * @return ShippingOptionEntity|null
+     */
+    public function getShippingOptions(string $id, Context $context): ?ShippingOptionEntity
+    {
+        $criteria = new Criteria([$id]);
+
+        return $this->shippingOptionsRepository->search($criteria, $context)->get($id);
     }
 
     /**
--- src/Storefront/Controller/ShippingOptionsController.php
+++ src/Storefront/Controller/ShippingOptionsController.php
@@ -1,16 +1,17 @@
 <?php
 
-
 namespace Kiener\KienerMyParcel\Storefront\Controller;
-
 
 use Exception;
 use Kiener\KienerMyParcel\Core\Content\ShippingOption\ShippingOptionEntity;
+use Kiener\KienerMyParcel\Service\Shipment\OrderService;
+use Kiener\KienerMyParcel\Service\ShippingOptions\ShippingOptionsService;
 use MyParcelNL\Sdk\src\Model\Consignment\AbstractConsignment;
 use MyParcelNL\Sdk\src\Model\Consignment\BpostConsignment;
 use MyParcelNL\Sdk\src\Model\Consignment\DPDConsignment;
 use MyParcelNL\Sdk\src\Model\Consignment\PostNLConsignment;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Core\PlatformRequest;
 use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,7 +21,12 @@ class ShippingOptionsController extends StorefrontController
 {
     public const ROUTE_NAME_CREATE = 'api.action.myparcel.shipping_options.create';
 
+    private const RESPONSE_KEY_SUCCESS = 'success';
+    private const RESPONSE_KEY_ERROR = 'error';
+    private const RESPONSE_KEY_SHIPPING_OPTIONS_ID = 'shipping_options_id';
+
     public const REQUEST_KEY_ORDER_ID = 'order_id';
+    public const REQUEST_KEY_ORDER_VERSION_ID = 'order_version_id';
     public const REQUEST_KEY_CARRIER_ID = 'carrier_id';
     public const REQUEST_KEY_AGE_CHECK = 'age_check';
     public const REQUEST_KEY_LARGE_FORMAT = 'large_format';
@@ -28,6 +34,31 @@ class ShippingOptionsController extends StorefrontController
     public const REQUEST_KEY_REQUIRES_SIGNATURE = 'requires_signature';
     public const REQUEST_KEY_ONLY_RECIPIENT = 'only_recipient';
     public const REQUEST_KEY_PACKAGE_TYPE = 'package_type';
+
+    /**
+     * @var OrderService
+     */
+    private $orderService;
+
+    /**
+     * @var ShippingOptionsService
+     */
+    private $shippingOptionsService;
+
+    /**
+     * ShippingOptionsController constructor.
+     *
+     * @param OrderService           $orderService
+     * @param ShippingOptionsService $shippingOptionsService
+     */
+    public function __construct(
+        OrderService $orderService,
+        ShippingOptionsService $shippingOptionsService
+    )
+    {
+        $this->orderService = $orderService;
+        $this->shippingOptionsService = $shippingOptionsService;
+    }
 
     /**
      * @RouteScope(scopes={"api"})
@@ -43,32 +74,60 @@ class ShippingOptionsController extends StorefrontController
      * @return JsonResponse
      * @throws Exception
      */
-    public function createForOrder(Request $request): ?ShippingOptionEntity
+    public function createForOrder(Request $request): JsonResponse
     {
-        if ((string)$request->get(self::REQUEST_KEY_ORDER_ID) === '') {
-            return null;
+        $orderId = $request->get(self::REQUEST_KEY_ORDER_ID);
+        $orderVersionId = $request->get(self::REQUEST_KEY_ORDER_VERSION_ID);
+
+        if ((string)$orderId === '' || (string)$orderVersionId === '') {
+            return new JsonResponse([
+                self::RESPONSE_KEY_SUCCESS => false,
+                self::RESPONSE_KEY_ERROR => sprintf(
+                    'Request is missing a valid %s or %s',
+                    self::REQUEST_KEY_ORDER_ID,
+                    self::REQUEST_KEY_ORDER_VERSION_ID
+                )
+            ]);
         }
 
-        $shippingOptions = new ShippingOptionEntity();
+        $context = $request->attributes->get(PlatformRequest::ATTRIBUTE_SALES_CHANNEL_CONTEXT_OBJECT);
+
+        $order = $this->orderService->getOrder($orderId,$orderVersionId,$context);
+
+        if ($order === null) {
+            return new JsonResponse([
+                self::RESPONSE_KEY_SUCCESS => false,
+                self::RESPONSE_KEY_ERROR => sprintf(
+                    'RCould not find an order with id %s and version id %s',
+                    $orderId,
+                    $orderVersionId
+                )
+            ]);
+        }
+
+        $shippingOptions = [
+            ShippingOptionEntity::FIELD_ORDER_ID => $orderId,
+            ShippingOptionEntity::FIELD_ORDER_VERSION_ID => $orderVersionId,
+        ];
 
         if ((string)$request->get(self::REQUEST_KEY_AGE_CHECK) !== '') {
-            $shippingOptions->setRequiresAgeCheck($request->get(self::REQUEST_KEY_AGE_CHECK));
+            $shippingOptions[ShippingOptionEntity::FIELD_REQUIRES_AGE_CHECK] = $request->get(self::REQUEST_KEY_AGE_CHECK);
         }
 
         if ((string)$request->get(self::REQUEST_KEY_LARGE_FORMAT) !== '') {
-            $shippingOptions->setLargeFormat($request->get(self::REQUEST_KEY_LARGE_FORMAT));
+            $shippingOptions[ShippingOptionEntity::FIELD_LARGE_FORMAT] = $request->get(self::REQUEST_KEY_LARGE_FORMAT);
         }
 
         if ((string)$request->get(self::REQUEST_KEY_RETURN_IF_NOT_HOME) !== '') {
-            $shippingOptions->setReturnIfNotHome($request->get(self::REQUEST_KEY_RETURN_IF_NOT_HOME));
+            $shippingOptions[ShippingOptionEntity::FIELD_RETURN_IF_NOT_HOME] = $request->get(self::REQUEST_KEY_RETURN_IF_NOT_HOME);
         }
 
         if ((string)$request->get(self::REQUEST_KEY_REQUIRES_SIGNATURE) !== '') {
-            $shippingOptions->setRequiresSignature($request->get(self::REQUEST_KEY_REQUIRES_SIGNATURE));
+            $shippingOptions[ShippingOptionEntity::FIELD_REQUIRES_SIGNATURE] = $request->get(self::REQUEST_KEY_REQUIRES_SIGNATURE);
         }
 
         if ((string)$request->get(self::REQUEST_KEY_ONLY_RECIPIENT) !== '') {
-            $shippingOptions->setOnlyRecipient($request->get(self::REQUEST_KEY_ONLY_RECIPIENT));
+            $shippingOptions[ShippingOptionEntity::FIELD_ONLY_RECIPIENT] = $request->get(self::REQUEST_KEY_ONLY_RECIPIENT);
         }
 
         $carrierId = $request->get(self::REQUEST_KEY_CARRIER_ID);
@@ -81,7 +140,7 @@ class ShippingOptionsController extends StorefrontController
                 PostNLConsignment::CARRIER_ID,
             ], true)
         ) {
-            $shippingOptions->setCarrierId($carrierId);
+            $shippingOptions[ShippingOptionEntity::FIELD_CARRIER_ID] = $carrierId;
         }
 
         $packageType = $request->get(self::REQUEST_KEY_PACKAGE_TYPE);
@@ -90,8 +149,18 @@ class ShippingOptionsController extends StorefrontController
             && is_int($packageType)
             && in_array($packageType, AbstractConsignment::PACKAGE_TYPES_IDS, true)
         ) {
-            $shippingOptions->setPackageType($packageType);
+            $shippingOptions[ShippingOptionEntity::FIELD_PACKAGE_TYPE] = $packageType;
         }
+
+        $shippingOptionsEntity = $this->shippingOptionsService->createOrUpdateShippingOptions(
+            $shippingOptions,
+            $context
+        );
+
+        return new JsonResponse([
+            self::RESPONSE_KEY_SUCCESS => $shippingOptionsEntity !== null,
+            self::RESPONSE_KEY_SHIPPING_OPTIONS_ID => $shippingOptionsEntity ? $shippingOptionsEntity->getId() : null
+        ]);
 
     }
 }
