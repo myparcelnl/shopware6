afc5dcdc5cd23dabfe1846f32a995504d6bb7aa4
Thijs Dekker <thijs@memo-ict.nl>
2020-12-08 14:09:38 +0100

    MP-4 fix bug

--- src/Subscriber/CheckoutConfirmPageSubscriber.php
+++ src/Subscriber/CheckoutConfirmPageSubscriber.php
@@ -4,6 +4,7 @@ namespace Kiener\KienerMyParcel\Subscriber;
 
 use Kiener\KienerMyParcel\Service\ShippingMethod\ShippingMethodService;
 use Kiener\KienerMyParcel\Setting\MyParcelSettingStruct;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Shopware\Storefront\Page\PageLoadedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Shopware\Storefront\Page\Checkout\Confirm\CheckoutConfirmPageLoadedEvent;
@@ -16,15 +17,23 @@ class CheckoutConfirmPageSubscriber implements EventSubscriberInterface
     private $shippingMethodService;
 
     /**
+     * @var SystemConfigService
+     */
+    private $configService;
+
+    /**
      * Creates a new instance of the checkout confirm page subscriber.
      *
      * @param ShippingMethodService $shippingMethodService
+     * @param SystemConfigService       $configService
      */
     public function __construct(
-        ShippingMethodService $shippingMethodService
+        ShippingMethodService $shippingMethodService,
+        SystemConfigService $configService
     )
     {
         $this->shippingMethodService = $shippingMethodService;
+        $this->configService = $configService;
     }
 
     /**
@@ -46,13 +55,32 @@ class CheckoutConfirmPageSubscriber implements EventSubscriberInterface
      */
     public function addMyParcelShippingMethodIdsToPage($args): void
     {
-        //dd($args);
-        $options = [
+
+        $data = [
             'myparcel_shipping_method_ids' => $this->shippingMethodService->getMyParcelShippingMethodIds(
                 $args->getContext()
             ),
         ];
 
-        $args->getPage()->assign($options);
+        if(isset($_COOKIE['myparcel-cookie-key'])){
+            $cookie_data = explode('_', $_COOKIE['myparcel-cookie-key']);
+
+            $data['myparcel_values'] = [
+                'shippingMethodId' => $cookie_data[0],
+                'deliveryDate'=> $cookie_data[1],
+                'deliveryType'=> $cookie_data[2],
+                'requiresSignature'=> $cookie_data[3],
+                'onlyRecipient'=> $cookie_data[4]
+            ];
+        }else{
+            $data['myparcel_values'] = [
+                'shippingMethodId' => $this->configService->get('KienerMyParcel.config.myParcelDefaultMethod'),
+                'deliveryDate'=> \date('Y-m-d'),
+                'deliveryType'=> $this->configService->get('KienerMyParcel.config.myParcelDefaultDeliveryWindow'),
+                'requiresSignature'=> $this->configService->get('KienerMyParcel.config.myParcelDefaultSignature'),
+                'onlyRecipient'=> $this->configService->get('KienerMyParcel.config.myParcelDefaultOnlyRecipient')
+            ];
+        }
+        $args->getPage()->assign($data);
     }
 }
