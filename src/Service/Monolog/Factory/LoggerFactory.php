8231f924487295116ab782a868c781d7ab579b96
Spencer Shaw <spencer@memo-ict.nl>
2022-05-19 11:57:44 +0200

    add: logger

---
+++ src/Service/Monolog/Factory/LoggerFactory.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace MyPa\Shopware\Service\Monolog\Factory;
+
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+use Shopware\Core\Framework\Log\Monolog\DoctrineSQLHandler;
+use Shopware\Core\Kernel;
+
+class LoggerFactory
+{
+
+    /**
+     * @param $filename
+     * @param $retentionDays
+     * @return RotatingFileHandler
+     */
+    public function createFileHandler($filename, $retentionDays): RotatingFileHandler
+    {
+        return new RotatingFileHandler($filename, $retentionDays, Logger::INFO);
+    }
+
+    /**
+     * @return DoctrineSQLHandler
+     */
+    public function createSQLHandler(): DoctrineSQLHandler
+    {
+        return new DoctrineSQLHandler(Kernel::getConnection(), Logger::INFO);
+    }
+
+}
