44b0047a1924d30d80881061de8b45ef47211cfa
Daniel Schipper <daniel@memo-ict.nl>
2022-08-29 14:48:55 +0200

    Remove legacy routes #3

--- src/Storefront/Controller/MyParcelController.php
+++ src/Storefront/Controller/MyParcelController.php
@@ -1,15 +1,10 @@
 <?php
-/**
- * @noinspection PhpUnused
- * @noinspection PhpUndefinedClassInspection
- */
 
 namespace MyPa\Shopware\Storefront\Controller;
 
 use Exception;
 use MyPa\Shopware\Facade\MyParcelFacade;
 use MyPa\Shopware\Service\Consignment\ConsignmentService;
-use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -17,16 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MyParcelController extends StorefrontController
 {
-    public const ROUTE_NAME_GET_CARRIERS = 'api.action.myparcel.carriers';
+    public const ROUTE_NAME_GET_CARRIERS      = 'api.action.myparcel.carriers';
     public const ROUTE_NAME_GET_PACKAGE_TYPES = 'api.action.myparcel.package_types';
 
-    /* For backwards compatibility with 6.3*/
-    public const ROUTE_NAME_GET_CARRIERS_LEGACY = 'api.action.myparcel.carriers';
-    public const ROUTE_NAME_GET_PACKAGE_TYPES_LEGACY = 'api.action.myparcel.package_types';
-    /* End backwards compatibility*/
-
-    private const RESPONSE_KEY_SUCCESS = 'success';
-    private const RESPONSE_KEY_CARRIERS = 'carriers';
+    private const RESPONSE_KEY_SUCCESS       = 'success';
+    private const RESPONSE_KEY_CARRIERS      = 'carriers';
     private const RESPONSE_KEY_PACKAGE_TYPES = 'package_types';
 
     /**
@@ -34,11 +24,11 @@ class MyParcelController extends StorefrontController
      */
     private $consignmentService;
 
-
     /**
      * MyParcelController constructor.
      *
      * @param ConsignmentService $consignmentService
+     * @param MyParcelFacade     $myParcelFacade
      */
     public function __construct(
         ConsignmentService $consignmentService,
@@ -48,8 +38,6 @@ class MyParcelController extends StorefrontController
         $this->consignmentService = $consignmentService;
         $this->myParcelFacade = $myParcelFacade;
     }
-
-
 
     /**
      * @RouteScope(scopes={"api"})
@@ -65,36 +53,9 @@ class MyParcelController extends StorefrontController
      */
     public function getCarriers(): JsonResponse
     {
-        return $this->getCarriersResponse();
-    }
-
-    /**
-     * @RouteScope(scopes={"api"})
-     * @Route(
-     *     "/api/v{version}/_action/myparcel/carriers",
-     *     defaults={"auth_enabled"=true},
-     *     name=MyParcelController::ROUTE_NAME_GET_CARRIERS_LEGACY,
-     *     methods={"GET"}
-     *     )
-     *
-     * @return JsonResponse
-     * @throws Exception
-     */
-    public function getCarriersLegacy(): JsonResponse
-    {
-        return $this->getCarriersResponse();
-    }
-
-    /**
-     * @return JsonResponse
-     * @throws Exception
-     */
-    private function getCarriersResponse(): JsonResponse
-    {
         return new JsonResponse([
-            self::RESPONSE_KEY_SUCCESS => true,
+            self::RESPONSE_KEY_SUCCESS  => true,
             self::RESPONSE_KEY_CARRIERS => $this->consignmentService->getCarrierIds(),
-
         ]);
     }
 
@@ -112,36 +73,9 @@ class MyParcelController extends StorefrontController
      */
     public function getPackageTypes(): JsonResponse
     {
-        return $this->getPackageTypesResponse();
-    }
-
-    /**
-     * @RouteScope(scopes={"api"})
-     * @Route(
-     *     "/api/v{version}/_action/myparcel/package_types",
-     *     defaults={"auth_enabled"=true},
-     *     name=MyParcelController::ROUTE_NAME_GET_PACKAGE_TYPES_LEGACY,
-     *     methods={"GET"}
-     *     )
-     *
-     * @return JsonResponse
-     * @throws Exception
-     */
-    public function getPackageTypesLegacy(): JsonResponse
-    {
-        return $this->getPackageTypesResponse();
-    }
-
-    /**
-     * @return JsonResponse
-     * @throws Exception
-     */
-    private function getPackageTypesResponse(): JsonResponse
-    {
         return new JsonResponse([
-            self::RESPONSE_KEY_SUCCESS => true,
+            self::RESPONSE_KEY_SUCCESS       => true,
             self::RESPONSE_KEY_PACKAGE_TYPES => $this->consignmentService->getPackageTypes(),
-
         ]);
     }
 }
