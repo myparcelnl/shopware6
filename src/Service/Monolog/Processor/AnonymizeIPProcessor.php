cc1766ee91a20cfc2c1e92c861b58a731b4e8e64
joerivanveen <joeri@myparcel.nl>
2023-08-28 17:45:14 +0200

    fix: remove signature for logrecord

--- src/Service/Monolog/Processor/AnonymizeIPProcessor.php
+++ src/Service/Monolog/Processor/AnonymizeIPProcessor.php
@@ -11,11 +11,11 @@ class AnonymizeIPProcessor implements ProcessorInterface
      * Gets an anonymous version of the
      * provided IP address string.
      *
-     * @param  array|\Monolog\LogRecord $record
+     * @param  array|\Monolog\LogRecord $record 6.4 expects array, 6.5 expects logRecord, minimum php is 7.4 and 8.1 respectively
      *
      * @return array
      */
-    public function __invoke(array|\Monolog\LogRecord $record)
+    public function __invoke($record)
     {
         if (!array_key_exists('ip', $record['extra'])) {
             return $record;
