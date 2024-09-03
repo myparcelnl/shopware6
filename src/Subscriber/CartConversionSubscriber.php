b5410470d65047304d6e133a0705ff19cb0501ff
Joeri van Veen <joeri@myparcel.nl>
2024-09-03 15:43:31 +0200

    feat: register pickup address in order when chosen

--- src/Subscriber/CartConversionSubscriber.php
+++ src/Subscriber/CartConversionSubscriber.php
@@ -9,6 +9,9 @@ use MyPa\Shopware\Service\Config\MyParcelCarriers;
 use MyParcelNL\Sdk\src\Model\Consignment\AbstractConsignment;
 use Psr\Log\LoggerInterface;
 use Shopware\Core\Checkout\Cart\Order\CartConvertedEvent;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class CartConversionSubscriber implements EventSubscriberInterface
@@ -38,16 +41,23 @@ class CartConversionSubscriber implements EventSubscriberInterface
     private $configReader;
 
     /**
+     * @var EntityRepository
+     */
+    private $countryRepository;
+
+    /**
      * @param LoggerInterface $logger
      * @param ConfigGenerator $configReader
+     * @param EntityRepository $countryRepository
      */
     public function __construct(
         LoggerInterface $logger,
-        ConfigGenerator $configReader
-    )
-    {
+        ConfigGenerator $configReader,
+        EntityRepository $countryRepository
+    ){
         $this->logger = $logger;
         $this->configReader = $configReader;
+        $this->countryRepository = $countryRepository;
     }
 
     /**
@@ -66,10 +76,11 @@ class CartConversionSubscriber implements EventSubscriberInterface
      */
     public function cartConverted(CartConvertedEvent $event)
     {
-        if (empty($event->getCart()->getExtension(Defaults::CART_EXTENSION_KEY))) {
+        $myParcelExtension = $event->getCart()->getExtension(Defaults::CART_EXTENSION_KEY);
+        if (null === $myParcelExtension) {
             return;
         }
-        $myParcelData = $event->getCart()->getExtension(Defaults::CART_EXTENSION_KEY)->getVars();
+        $myParcelData = $myParcelExtension->getVars();
         $options = $this->setGeneralDefaults($event->getSalesChannelContext()->getSalesChannelId());
 
         //Cart extension data check if it is a myparcel shipping
@@ -97,21 +108,23 @@ class CartConversionSubscriber implements EventSubscriberInterface
                         //Check if the option was even shown via 'allowSignature' and 'allowOnlyRecipient', otherwise use the given value
                         $carrierId = $deliveryData['carrier'];
 
-                        if ($this->configReader->isSettingEnabled(
+                        if (isset($value['only_recipient'])
+                            && $this->configReader->isSettingEnabled(
                                 $event->getSalesChannelContext()->getSalesChannelId(),
                                 'allowOnlyRecipient',
                                 MyParcelCarriers::NPM_CARRIER_TO_CONFIG_CARRIER[$carrierId]
-                            ) &&
-                            isset($value['only_recipient'])) {
+                            )
+                        ) {
                             $options[ShippingOptionEntity::FIELD_ONLY_RECIPIENT] = $value['only_recipient'];
                         }
 
-                        if ($this->configReader->isSettingEnabled(
+                        if (isset($value['signature'])
+                            && $this->configReader->isSettingEnabled(
                                 $event->getSalesChannelContext()->getSalesChannelId(),
                                 'allowSignature',
                                 MyParcelCarriers::NPM_CARRIER_TO_CONFIG_CARRIER[$carrierId]
-                            ) &&
-                            isset($value['signature'])) {
+                            )
+                        ) {
                             $options[ShippingOptionEntity::FIELD_REQUIRES_SIGNATURE] = $value['signature'];
                         }
                         break;
@@ -121,7 +134,7 @@ class CartConversionSubscriber implements EventSubscriberInterface
                         break;
 
                     case 'pickupLocation':
-                        $options[ShippingOptionEntity::FIELD_PICKUP_LOCATION_ID] = intval($value['location_code']);
+                        $options[ShippingOptionEntity::FIELD_PICKUP_LOCATION_ID] = (int) $value['location_code'];
                         $options[ShippingOptionEntity::FIELD_PICKUP_NAME] = $value['location_name'];
                         $options[ShippingOptionEntity::FIELD_PICKUP_STREET] = $value['street'];
                         $options[ShippingOptionEntity::FIELD_PICKUP_NUMBER] = $value['number'] . $value['number_suffix'];
@@ -129,6 +142,7 @@ class CartConversionSubscriber implements EventSubscriberInterface
                         $options[ShippingOptionEntity::FIELD_PICKUP_CITY] = $value['city'];
                         $options[ShippingOptionEntity::FIELD_PICKUP_CC] = $value['cc'];
                         $options[ShippingOptionEntity::FIELD_RETAIL_NETWORK_ID] = $value['retail_network_id'];
+                        $this->setShippingAddress($event, $options);
                         break;
                     case 'packageType':
                         $options[ShippingOptionEntity::FIELD_PACKAGE_TYPE] = AbstractConsignment::PACKAGE_TYPES_NAMES_IDS_MAP[$value ?? 'package'];
@@ -177,5 +191,45 @@ class CartConversionSubscriber implements EventSubscriberInterface
             }
         }
         return $options;
+    }
+
+    /**
+     * @param CartConvertedEvent $event
+     * @param $value
+     * @return void
+     */
+    protected function setShippingAddress(CartConvertedEvent $event, array $options): void
+    {
+        $cart = $event->getConvertedCart();
+
+        // copy shippingOrderAddress to addresses when no separate billing address is used
+        $billingAddressId = $cart['billingAddressId'];
+        $orderAddress = $cart['deliveries'][0]['shippingOrderAddress'];
+
+        if (!isset($cart['addresses'])) {
+            $cart['addresses'] = [$orderAddress];
+        } elseif (!array_filter($cart['addresses'], function ($address) use ($billingAddressId) {
+            return $address['id'] === $billingAddressId;
+        })) {
+            $cart['addresses'][] = $orderAddress;
+        }
+
+        // find country by country code iso 2
+        $country = $this->countryRepository->search(
+            (new Criteria())->addFilter(new EqualsFilter('iso', $options[ShippingOptionEntity::FIELD_PICKUP_CC])),
+            $event->getSalesChannelContext()->getContext()
+        )->first();
+
+        // manipulate shipping address, remove id to prevent it being stored in the account
+        $cart['deliveries'][0]['shippingOrderAddress']['id'] = null;
+        $cart['deliveries'][0]['shippingOrderAddress']['company'] = $options[ShippingOptionEntity::FIELD_PICKUP_NAME];
+        $cart['deliveries'][0]['shippingOrderAddress']['department'] = 'pickup point';
+        $cart['deliveries'][0]['shippingOrderAddress']['street'] = $options[ShippingOptionEntity::FIELD_PICKUP_STREET] . ' ' . $options[ShippingOptionEntity::FIELD_PICKUP_NUMBER];
+        $cart['deliveries'][0]['shippingOrderAddress']['zipcode'] = $options[ShippingOptionEntity::FIELD_PICKUP_POSTAL_CODE];
+        $cart['deliveries'][0]['shippingOrderAddress']['city'] = $options[ShippingOptionEntity::FIELD_PICKUP_CITY];
+        if (isset($country)) {
+            $cart['deliveries'][0]['shippingOrderAddress']['countryId'] = $country->getId();
+        }
+        $event->setConvertedCart($cart);
     }
 }
